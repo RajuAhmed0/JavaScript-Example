69e8cf77f21aa0a8626b143b930c8c75f64dd68b
Your Name <you@example.com>
2024-06-28 12:55:56 +0600

    first commit

---
+++ example-js.js
@@ -0,0 +1,39 @@
+// varible
+
+
+//  number
+
+var price = 500;
+var sajib_age = 20;
+// var mehedi_number  =  ;
+var SajibArGariAse = 10;
+var bike_speed = 200;
+
+
+// String
+
+var brotherName = sajib;
+var gfName = sadiya;
+var rajuAge = 21;
+var address = vorotpur, boraigram, natore;
+var fileName = lorem;
+// var email  = abss@gmail.com;
+var collageName = Mirpur_collage;
+var trainer = asif_iqbal;
+var yourFavoriteGame  = football;
+
+
+// Boolean
+
+var youEatingRice   = true;
+var areYouGoToSchool  = false;
+var youLikeFootball  = true ;
+
+
+// Don't use this item 
+
+var 
+
+  
+
+
